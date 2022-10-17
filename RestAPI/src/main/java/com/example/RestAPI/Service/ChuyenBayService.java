65337b4a0c6fd704c0f626f9085874101d111e49
Huy Tong <tonghuy214@gmail.com>
2022-10-17 13:44:22 +0700

    add function chuyenbay

--- RestAPI/src/main/java/com/example/RestAPI/Service/ChuyenBayService.java
+++ RestAPI/src/main/java/com/example/RestAPI/Service/ChuyenBayService.java
@@ -15,16 +15,16 @@ import com.example.RestAPI.Entity.ChuyenBay;
 public interface ChuyenBayService {
 	public List<ChuyenBay> listChuyenBay();
 
-//	// 1.Cho biết các chuyến bay đi Đà Lạt (DAD)
-//	public List<ChuyenBay> chuyenBayDenDAD(String gaDen);
-//
-//	// 4. Cho biết các chuyến bay có độ dài đường bay nhỏ hơn 10.000km và lớn hơn
-//	// 8.000km
-//	public List<ChuyenBay> doDaiNhoHon10kVaLonHon8k();
-//
-//	// 5. Cho biết các chuyến bay xuất phát từ Sài Gòn (SGN) đi Ban Mê Thuộc (BMV)
-//	public List<ChuyenBay> chuyenBaySgDenBmt(String gaDi, String gaDen);
-//
-//	// 6. Có bao nhiêu chuyến bay xuất phát từ Sài Gòn (SGN)
-//	public Integer countChuyenBayTuSG(String gaDi);
+	// 1.Cho biết các chuyến bay đi Đà Lạt (DAD)
+	public List<ChuyenBay> chuyenBayDenDAD();
+
+	// 4. Cho biết các chuyến bay có độ dài đường bay nhỏ hơn 10.000km và lớn hơn
+	// 8.000km
+	public List<ChuyenBay> doDaiNhoHon10kVaLonHon8k();
+
+	// 5. Cho biết các chuyến bay xuất phát từ Sài Gòn (SGN) đi Ban Mê Thuộc (BMV)
+	public List<ChuyenBay> chuyenBaySgDenBmt(String gaDi, String gaDen);
+
+	// 6. Có bao nhiêu chuyến bay xuất phát từ Sài Gòn (SGN)
+	public Integer countChuyenBayTuSG(String gaDi);
 }
