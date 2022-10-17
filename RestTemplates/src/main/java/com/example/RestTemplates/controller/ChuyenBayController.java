e798abae6c3750b2565b4a8fa8f64c43421b20b7
Huy Tong <tonghuy214@gmail.com>
2022-10-17 13:06:33 +0700

    finish show list chuyen bay with RestTemplates

---
+++ RestTemplates/src/main/java/com/example/RestTemplates/controller/ChuyenBayController.java
@@ -0,0 +1,33 @@
+package com.example.RestTemplates.controller;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api")
+public class ChuyenBayController {
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/templates/chuyenbay")
+	public String getChuyenBays() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+		ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/chuyenbay", HttpMethod.GET,entity,String.class);
+		return responseEntity.getBody();
+	}
+}
